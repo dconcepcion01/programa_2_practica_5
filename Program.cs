e328d64d003cd349673448f91c0cf0dc0fa8725b
Danny <dannyconcepcion01@gmail.com>
2020-09-20 20:26:34 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_2_practica_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Arrays
+
+            int[] notas = new int[4];
+            notas[0] = 90;
+            notas[1] = 88;
+            notas[2] = 92;
+            notas[3] = 79; 
+
+            //iterando las calificaciones
+            
+            for (int i = 0; i <notas.Length; i++)
+            {
+                Console.Write($"Ingrese nota{1+1}: ");
+                notas[i] = int.Parse(Console.ReadLine());
+                
+
+            }
+            for (int i = 0; i < notas.Length; i++)
+
+                Console.WriteLine(notas[i]);
+
+         
+
+            {
+
+            }
+            Console.ReadKey();
+
+             
+
+        }
+    }
+}
